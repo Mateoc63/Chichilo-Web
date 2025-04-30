50e4e2d88cc2c2e7767abf49274908d1c5a2a3e5
Mateo <mateoc36@hotmail.com>
2025-04-29 21:21:58 -0300

    Retoques en el codigo

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-// Seleccionamos todos los botones de "Ver detalles"
 const detailsBtns = document.querySelectorAll('.details-btn');
-
-// Recorremos los botones para agregar el evento
 detailsBtns.forEach(button => {
     button.addEventListener('click', function() {
-        const menu = this.nextElementSibling; // El div dropdown-menu correspondiente al botón
+        const menu = this.nextElementSibling; 
 
-        // Si el menú ya está visible, lo ocultamos
         if (menu.style.display === 'block') {
             menu.style.display = 'none';
         } else {
-            // Primero, ocultamos todos los menús desplegables abiertos
             const allMenus = document.querySelectorAll('.dropdown-menu');
             allMenus.forEach(m => m.style.display = 'none');
-            
-            // Luego, mostramos el menú actual
             menu.style.display = 'block';
         }
     });
@@ -24,5 +17,5 @@ const hamburgerBtn = document.getElementById('hamburger-btn');
 const navLinks = document.getElementById('nav-links');
 
 hamburgerBtn.addEventListener('click', () => {
-    navLinks.classList.toggle('active');  // Alterna la visibilidad del menú
+    navLinks.classList.toggle('active'); 
 });
